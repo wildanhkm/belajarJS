f41c4893b19a239edf2c82defd503524b5c0d2ef
wildanhkm <wildanhakim98@gmail.com>
2021-03-06 18:03:41 +0700

    new react excercise: file tree gadget app

--- react-js/file-tree-gadget/src/App.js
+++ react-js/file-tree-gadget/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  //this is a component. A component is the basic unit in React,
+  //so React component is a javascript function that return something to be rendered on the screen
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Folder name="Desktop">
+        <Folder name="Music">
+          <File name="all_star.mp4" />
+          <File name="cat_meow.mp4" />
+        </Folder>
+        <File name="cat_photo.jpg" />
+        <File name="tokyo.png" />
+      </Folder>
+      <Folder name="Applications" />
     </div>
   );
 }
+
+const Folder = (props) => {
+  //This is an arrow function
+  useState(false); //useState is a function
+  const [isOpen, setIsOpen] = useState(true);
+  const direction = isOpen ? "down" : "right";
+  const { name, children } = props;
+
+  const handleClick = () => setIsOpen(!isOpen);
+  return (
+    <div>
+      <span onClick={handleClick}>
+        <i className="blue folder icon"></i>
+        <i className={`caret ${direction} icon`}></i>
+      </span>
+      {name}
+      <div style={{ marginLeft: "18px" }}>{isOpen ? children : null}</div>
+    </div>
+  );
+};
+
+const File = (props) => {
+  const { name } = props;
+  const fileExtension = name.split(".")[1];
+  const fileIcons = {
+    mp4: "headphones",
+    jpg: "file image",
+    png: "file image outline",
+  };
+  return (
+    <div>
+      <i className={`${fileIcons[fileExtension]} icon`}></i>
+      {name}
+    </div>
+  );
+};
 
 export default App;
--- react-js/my-app/src/App.js
+++ react-js/my-app/src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './App.css';
-import ReviewItems from './Reviews';
-import PropTypes from 'prop-types';
+import React from "react";
+import "./App.css";
+import ReviewItems from "./Reviews";
+import propTypes from "prop-types";
 
 function App() {
   return (
     <div className="Parentbox">
-      <FotoProduk/>
-      <ProdukInfo name="Any Shoes" category="LEBARAN" isDiscount="yes"/>
-      <ReviewItems/>
+      <FotoProduk />
+      <ProdukInfo name="Any Shoes" category="LEBARAN" isDiscount="yes" />
+      <ReviewItems />
     </div>
   );
 }
 
-function FotoProduk(){
+function FotoProduk() {
   return (
     <div className="Foto">
-      <img src="bwasneaker.jpg" alt="produk"/>
+      <img src="bwasneaker.jpg" alt="produk" />
     </div>
   );
 }
@@ -24,49 +24,52 @@ function FotoProduk(){
 function CekDiskon(props) {
   const { isDiscount, discount } = props;
   if (isDiscount) {
-    return (
-      <p>Diskon {discount}%</p>
-    );
-  }else{
-    return (
-      <p>Tidak ada diskon</p>
-    );
+    return <p>Diskon {discount}%</p>;
+  } else {
+    return <p>Tidak ada diskon</p>;
   }
 }
 
-function ProdukInfo(props){
-  const {category, name, isDiscount} = props;
-  const benefits = ["Bahan adem dan nyaman", "Sol dan jahitan kuat", "Model klasik", "Diskon", "Keren"];
-  const listBenefits = benefits.map((itemBenefit) => 
+function ProdukInfo(props) {
+  const { category, name, isDiscount } = props;
+  const benefits = [
+    "Bahan adem dan nyaman",
+    "Sol dan jahitan kuat",
+    "Model klasik",
+    "Diskon",
+    "Keren",
+  ];
+  const listBenefits = benefits.map((itemBenefit) => (
     <li key={itemBenefit}>{itemBenefit}</li>
-  );
+  ));
   return (
     <div>
       <div className="Deskripsi">
         <p className="Cate">{category}</p>
         <h1 className="Title">{name}</h1>
         <p className="Price">IDR 2.999.999</p>
-        <CekDiskon isDiscount={isDiscount}  discount={50} />
+        <CekDiskon isDiscount={isDiscount} discount={50} />
         <p className="Info">
-          One of the most recognizable shoes in the AJ Collection, the Air Jordan 3 Retro features lightweight,
-          visible cushioning just like the original from the '88. Signature details and materials celebrate
-          the game-changing icon.
+          One of the most recognizable shoes in the AJ Collection, the Air
+          Jordan 3 Retro features lightweight, visible cushioning just like the
+          original from the '88. Signature details and materials celebrate the
+          game-changing icon.
         </p>
-        <ul>
-          {listBenefits}
-        </ul>
-        <a onClick={(e) => TambahCart(name, e)} href="#">Add to cart</a>
+        <ul>{listBenefits}</ul>
+        <a onClick={(e) => TambahCart(name, e)} href="#">
+          Add to cart
+        </a>
       </div>
     </div>
   );
 }
 
-function TambahCart(e){
+function TambahCart(e) {
   return console.log("Membeli produk " + e);
 }
 
 CekDiskon.propTypes = {
-  discount: PropTypes.number.isRequired
+  discount: propTypes.number.isRequired,
 };
 
 export default App;
