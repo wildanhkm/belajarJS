38d1219c8ec71b6ef9afd36ab3eca29c046a9698
wildanhkm <wildanhakim98@gmail.com>
2021-03-19 21:58:45 +0700

    learn vanilla js function

---
+++ vanilla-js/function.js
@@ -0,0 +1,9 @@
+function hitungVolumeKubus(a) {
+  let volume = a ** 3;
+  return volume;
+}
+
+let a = hitungVolumeKubus(8);
+let b = hitungVolumeKubus(3);
+let jumlah = a + b;
+console.log(jumlah);
