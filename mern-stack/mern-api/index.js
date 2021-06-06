2b355256ea6ee0d68d38a7b543f6b428dca2579f
wildanhkm <wildanhakim98@gmail.com>
2021-06-06 21:04:58 +0700

    planning api, routing, and add controller

--- mern-stack/mern-api/index.js
+++ mern-stack/mern-api/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const productRoutes = require("./src/routes/products");
 
-app.use(() => {
-  console.log("Hello server");
+app.use(bodyParser.json()); //* type JSON
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
+
+app.use("/", productRoutes);
 app.listen(4000);
---
+++ mern-stack/mern-api/src/controllers/products.js
@@ -0,0 +1,27 @@
+exports.createProduct = (req, res, next) => {
+  const name = req.body.name;
+  const price = req.body.price;
+  res.json({
+    message: "Create Product Success",
+    data: {
+      id: 1,
+      name: name,
+      price: price,
+    },
+  });
+  next();
+};
+
+exports.getAllProducts = (req, res, next) => {
+  res.json({
+    message: "Get All Products Success",
+    data: [
+      {
+        id: 1,
+        name: "Sari Gandum",
+        price: 8000,
+      },
+    ],
+  });
+  next();
+};
