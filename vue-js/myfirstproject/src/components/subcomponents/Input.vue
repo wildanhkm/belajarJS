30cac5553b36ff2d176f76f1a364564beba0fe2f
wildanhkm <wildanhakim98@gmail.com>
2021-06-12 21:53:39 +0700

    learn vue cli, components, subcomponents, props

---
+++ vue-js/myfirstproject/src/components/subcomponents/Input.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="type === text">
+    <input type="text" name="text" />
+  </div>
+  <div v-else-if="type === date">
+    <input type="datetime-local" name="date" />
+  </div>
+  <div v-else>
+    <input type="number" name="number" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    type: String
+  }
+};
+</script>
+
+<style scoped>
+input {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+}
+</style>
