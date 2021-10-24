8826b14ad355c28a36b5599f11fc33eab497e4b9
wildanhkm <wildanhakim98@gmail.com>
2021-10-24 13:27:47 +0700

    mern stack update handle image file upload

--- mern-stack/mern-api/src/controllers/blog.js
+++ mern-stack/mern-api/src/controllers/blog.js
@@ -2,10 +2,6 @@ const { validationResult } = require("express-validator");
 const BlogPost = require("../models/blog");
 
 exports.createBlogPost = (req, res, next) => {
-  const title = req.body.title;
-  // const image = req.body.image;
-  const body = req.body.body;
-
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -15,9 +11,20 @@ exports.createBlogPost = (req, res, next) => {
     throw error;
   }
 
+  if (!req.file) {
+    const error = new Error("Image not uploaded and should be uploaded");
+    error.errorStatus = 422;
+    throw error;
+  }
+
+  const title = req.body.title;
+  const image = req.file.path;
+  const body = req.body.body;
+
   const Posting = new BlogPost({
     title: title,
     body: body,
+    image: image,
     author: { uid: 1, name: "Wildan Hakim" },
   });
 
