ecf049877a365b0607efc59e3d80a66a3a647c2e
wildanhkm <wildanhakim98@gmail.com>
2021-10-28 19:43:51 +0700

    update: replace dummy data in react with response

--- mern-stack/mern-blog/src/components/molecules/BlogItem/index.js
+++ mern-stack/mern-blog/src/components/molecules/BlogItem/index.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { RegisterBg } from "../../../assets";
-import { Button } from "../../atoms";
+import { Button, Gap } from "../../atoms";
 import { useHistory } from "react-router-dom";
 import "./blogItem.scss";
 
-const BlogItem = () => {
+const BlogItem = (props) => {
+  const { image, title, date, name, body } = props;
   const history = useHistory();
   return (
     <div className="blog-item">
-      <img className="image-thumbnail" src={RegisterBg} alt="blog thumbnail" />
+      <img className="image-thumbnail" src={image} alt="blog thumbnail" />
       <div className="content-detail">
-        <p className="title">Title</p>
-        <p className="author">Author - Date post</p>
-        <p className="body">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-          laboriosam pariatur esse explicabo consequatur omnis veniam aut
-          repellat, possimus eius.
+        <p className="title">{title}</p>
+        <p className="author">
+          {name} - {date}
         </p>
+        <p className="body">{body}</p>
+        <Gap height={20} />
         <Button
           title="View Detail"
           onClick={() => history.push("/detail-blog")}
--- mern-stack/mern-blog/src/pages/Home/index.js
+++ mern-stack/mern-blog/src/pages/Home/index.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import { BlogItem, Button, Gap } from "../../components";
 import "./home.scss";
 import { useHistory } from "react-router-dom";
 
 const Home = () => {
+  const [dataBlog, setDataBlog] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:4000/v1/blog/posts")
+      .then((result) => {
+        console.log("data api", result.data);
+        const responseApi = result.data;
+
+        setDataBlog(responseApi.data);
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  }, []);
   const history = useHistory();
   return (
     <div className="homepage-container">
@@ -15,10 +29,18 @@ const Home = () => {
       </div>
       <Gap height={20} />
       <div className="content-container">
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
-        <BlogItem />
+        {dataBlog.map((blog) => {
+          return (
+            <BlogItem
+              key={blog._id}
+              image={`http://localhost:4000/${blog.image}`}
+              title={blog.title}
+              body={blog.body}
+              name={blog.author.name}
+              date={blog.createdAt}
+            />
+          );
+        })}
       </div>
       <div className="pagination">
         <Button title="Previous" />
